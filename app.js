d9fa01377d8ea4bac9ab8ebe7db9ccaed2605a2a
Tavish <YoTavishTayal@gmail.com>
2022-06-03 09:27:03 +0530

    Sangeet

---
+++ app.js
@@ -0,0 +1,55 @@
+
+function heatwaves() {
+    let audio = new Audio('sometimes all i think about is you.mp3')
+    audio.play();
+}
+
+function Style() {
+    let audio = new Audio('Style.mp3')
+    audio.play();
+}   
+
+function Dil() {
+    let audio = new Audio('dil.mp3')
+    audio.play();
+}
+
+function Pasoori() {
+    let audio = new Audio('Pasoori.mp3')
+    audio.play();
+}
+
+function Sad() {
+    let audio = new Audio('Sad.mp3')
+    audio.play();
+}
+
+function stop1() {
+    let audio = new Audio('sometimes all i think about is you.mp3')
+    audio.pause();
+}
+
+function stop2() {
+    let audio = new Audio('Style.mp3')
+    audio.pause();
+}   
+
+function stop3() {
+    let audio = new Audio('dil.mp3')
+    audio.pause();
+}
+
+function stop4() {
+    let audio = new Audio('Pasoori.mp3')
+    audio.pause();
+}
+
+function stop5() {
+    let audio = new Audio('Sad.mp3')
+    audio.pause();
+}
+
+
+
+
+
